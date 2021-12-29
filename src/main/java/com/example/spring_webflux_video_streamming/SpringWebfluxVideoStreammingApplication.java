f80def401442782c68963efb252e5ab095a83655
Amimul Ehsan <1805056@ugrad.cse.buet.ac.bd>
2021-12-29 13:14:26 +0600

    First Commit

---
+++ src/main/java/com/example/spring_webflux_video_streamming/SpringWebfluxVideoStreammingApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_webflux_video_streamming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxVideoStreammingApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run(SpringWebfluxVideoStreammingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_webflux_video_streamming/SpringWebfluxVideoStreammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_webflux_video_streamming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxVideoStreammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
